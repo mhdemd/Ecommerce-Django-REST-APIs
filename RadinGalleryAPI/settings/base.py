116ad449d8e8d6e8bca416dc8089fdb41a4498aa
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-11-28 13:39:22 +0330

    Add DRF settings

--- RadinGalleryAPI/settings/base.py
+++ RadinGalleryAPI/settings/base.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External apps (third-party or installed packages)
+    "rest_framework",
+    # Internal apps (developed in-house)
 ]
 
 MIDDLEWARE = [
@@ -105,3 +108,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
