3c27696bf7d5aaa235561b4ea170915a12e7d4c9
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-04 12:39:06 +0330

    Update comments

--- authentication/tests/test_LogoutView.py
+++ authentication/tests/test_LogoutView.py
@@ -1,30 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from authentication.models import (
-    User,  # به‌روزرسانی مسیر واردات مدل کاربر بر اساس پروژه شما
-)
+User = get_user_model()
 
 
 class LogoutViewTest(TestCase):
     def setUp(self):
-        # ایجاد کاربر برای تست
+        # Create a user for testing
         self.user = User.objects.create_user(
             username="testuser", password="testpassword", email="test@example.com"
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-        # ایجاد توکن معتبر برای تست
+        # Create a valid token for testing
         self.refresh_token = RefreshToken.for_user(self.user)
 
-        # مسیر درخواست برای لاگ‌اوت
+        # Request path for logout
         self.logout_url = "/api/logout/"
 
     def test_successful_logout(self):
-        """تست درخواست POST با توکن معتبر"""
+        """Test POST request with valid token"""
         response = self.client.post(
             self.logout_url, {"refresh": str(self.refresh_token)}
         )
@@ -32,20 +31,20 @@ class LogoutViewTest(TestCase):
         self.assertEqual(response.data["message"], "Logged out successfully.")
 
     def test_invalid_token_logout(self):
-        """تست درخواست POST با توکن نامعتبر"""
+        """Test POST request with invalid token"""
         response = self.client.post(self.logout_url, {"refresh": "invalidtoken123"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["error"], "Invalid token.")
 
     def test_missing_token_logout(self):
-        """تست درخواست POST بدون ارسال توکن"""
+        """Test POST request without sending token"""
         response = self.client.post(self.logout_url, {})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertTrue("error" in response.data)
 
     def test_logout_server_error(self):
-        """تست درخواست POST که منجر به خطای سرور می‌شود"""
-        # شبیه‌سازی خطای سرور با درخواست POST بدون توکن
+        """POST request test that results in a server error"""
+        # Simulate a server error with a POST request without a token
         with self.assertRaises(Exception):
             response = self.client.post(self.logout_url, {"refresh": "somevalidtoken"})
             self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
