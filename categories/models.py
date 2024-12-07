a9de9e8cbf3517f62b396cb3ffd1f6b58a7eb963
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-07 10:58:40 +0330

    Add models to all Apps

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=_("Category Name"),
+        help_text=_("Required, max 100 characters"),
+    )
+    slug = models.SlugField(
+        max_length=150,
+        unique=True,
+        verbose_name=_("Category Safe URL"),
+        help_text=_("Required, letters, numbers, underscore, or hyphens"),
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name=_("Is Active"),
+        help_text=_("Designates whether this category is active."),
+    )
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.PROTECT,
+        related_name="children",
+        verbose_name=_("Parent of Category"),
+        help_text=_("Not required"),
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Date Category Created"),
+        help_text=_("Format: Y-m-d H:M:S"),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("Date Category Updated"),
+        help_text=_("Format: Y-m-d H:M:S"),
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    class Meta:
+        ordering = ["name"]
+        indexes = [
+            models.Index(fields=["name"]),
+            models.Index(fields=["slug"]),
+        ]
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+        unique_together = ("slug", "parent")
+
+    def get_absolute_url(self):
+        return reverse("category_detail", args=[self.slug])
+
+    def __str__(self):
+        return self.name
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,37 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from products.models.product import Product
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("User"),
+        help_text=_("The user who added the product to the wishlist."),
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name=_("Product"),
+        help_text=_("The product added to the wishlist."),
+    )
+    added_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Date Added"),
+        help_text=_("The date and time when the product was added to the wishlist."),
+    )
+
+    class Meta:
+        unique_together = ("user", "product")
+        verbose_name = _("Wishlist Item")
+        verbose_name_plural = _("Wishlist Items")
+        indexes = [
+            models.Index(fields=["user"]),
+            models.Index(fields=["product"]),
+        ]
+
+    def __str__(self):
+        return f"{self.user.username}'s wishlist item: {self.product.name}"
