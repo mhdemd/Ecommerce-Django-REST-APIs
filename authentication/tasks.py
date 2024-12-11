905fe272c58764760e05ac319e1cc1a6ace50abb
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-11 11:32:43 +0330

    update task.py for ForgotPasswordView

--- authentication/tasks.py
+++ authentication/tasks.py
@@ -2,10 +2,25 @@ from celery import shared_task
 from django.core.mail import send_mail
 
 
+# ---------------------------------------------------------
+# RegisterView
+# ---------------------------------------------------------
 @shared_task
 def send_verification_email(subject, message, from_email, recipient_list):
     try:
         send_mail(subject, message, from_email, recipient_list)
         return f"Email sent to {recipient_list}"
+    except Exception as e:
+        return f"Failed to send email: {e}"
+
+
+# ---------------------------------------------------------
+# ForgotPasswordView
+# ---------------------------------------------------------
+@shared_task
+def send_reset_password_email(subject, message, from_email, recipient_list):
+    try:
+        send_mail(subject, message, from_email, recipient_list)
+        return f"Password reset email sent to {recipient_list}"
     except Exception as e:
         return f"Failed to send email: {e}"
