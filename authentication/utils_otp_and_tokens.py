e25199928cb913bd4610bc654b0ffee3441c4371
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-12 21:39:01 +0330

    Add def generate_verification_token

--- authentication/utils_otp_and_tokens.py
+++ authentication/utils_otp_and_tokens.py
@@ -10,3 +10,8 @@ from .redis_utils import delete_from_redis, get_from_redis, save_to_redis
 def generate_otp():
     # Generate a 6-digit numeric OTP
     return f"{random.randint(100000, 999999)}"
+
+
+def generate_verification_token():
+    # Generate a random 32-char token
+    return get_random_string(32)
