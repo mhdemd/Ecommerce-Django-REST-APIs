bfb4c69f0e6c7ff18cd6aec03964a3584c628665
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-12 21:38:40 +0330

    Add def generate_otp

--- authentication/utils_otp_and_tokens.py
+++ authentication/utils_otp_and_tokens.py
@@ -5,3 +5,8 @@ from django.utils.crypto import get_random_string
 from django.utils.timezone import now, timedelta
 
 from .redis_utils import delete_from_redis, get_from_redis, save_to_redis
+
+
+def generate_otp():
+    # Generate a 6-digit numeric OTP
+    return f"{random.randint(100000, 999999)}"
