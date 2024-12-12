b3ffa6f1aeb4e0575a684b63e17d571e9191a740
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-12 21:36:05 +0330

    get_from_redis and delete_from_redis

--- authentication/redis_utils.py
+++ authentication/redis_utils.py
@@ -8,3 +8,13 @@ def save_to_redis(key, value, ttl=None):
         settings.redis_instance.setex(key, ttl, value)
     else:
         settings.redis_instance.set(key, value)
+
+
+def get_from_redis(key):
+    # Retrieve the value associated with the given key from Redis.
+    return settings.redis_instance.get(key)
+
+
+def delete_from_redis(key):
+    # Delete a key-value pair from Redis.
+    settings.redis_instance.delete(key)
