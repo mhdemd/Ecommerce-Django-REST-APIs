4f2ee65594a69fd6e06b6ec045e36d6fd8cf331f
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-12 21:43:02 +0330

    token_mixin.py DONE

--- authentication/token_mixin.py
+++ authentication/token_mixin.py
@@ -6,3 +6,23 @@ from django.utils.timezone import now, timedelta
 from .utils_otp_and_tokens import store_verification_token
 
 logger = logging.getLogger(__name__)
+
+
+class TokenMixin:
+    def generate_token(self, user, expiry_hours=1):
+        # Generate a secure token and store it in Redis with an expiration time
+        token = get_random_string(32)
+        token_expiration = now() + timedelta(hours=expiry_hours)
+
+        # Convert expiration to seconds
+        ttl = int((token_expiration - now()).total_seconds())
+
+        # Store the token with the user_id in Redis
+        # Redis will handle expiration automatically
+        store_verification_token(token, user.id, ttl=ttl)
+
+        logger.info(
+            f"Token for user {user.id} generated and stored in Redis with TTL {ttl} seconds."
+        )
+
+        return token
