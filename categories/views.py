2ca697a48ddc3f29b63248828bca2d3b7fcfe52d
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-14 12:16:32 +0330

    category view add imports

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Category
+from .permissions import IsAdminUserOrReadOnly
+from .serializers import CategorySerializer
