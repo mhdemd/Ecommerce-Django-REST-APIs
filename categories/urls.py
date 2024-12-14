6c23e71580ec9ac11caa71e7ea0abe8e50117804
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-14 12:14:13 +0330

    category urls urlpatterns DONE

--- categories/urls.py
+++ categories/urls.py
@@ -6,3 +6,20 @@ from .views import (  # User Endpoints; Admin Endpoints
     CategoryDetailView,
     CategoryListView,
 )
+
+urlpatterns = [
+    # User Endpoints (Categories)
+    path("categories/", CategoryListView.as_view(), name="category-list"),
+    path("categories/<int:pk>/", CategoryDetailView.as_view(), name="category-detail"),
+    # Admin Endpoints (Categories)
+    path(
+        "admin/categories/",
+        AdminCategoryListCreateView.as_view(),
+        name="admin-category-list-create",
+    ),
+    path(
+        "admin/categories/<int:pk>/",
+        AdminCategoryDetailView.as_view(),
+        name="admin-category-detail",
+    ),
+]
