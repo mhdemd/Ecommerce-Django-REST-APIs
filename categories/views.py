c67d6ed254df35fb57b3e7688095454ec1063fd8
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-14 12:17:34 +0330

    Add CategoryListView class

--- categories/views.py
+++ categories/views.py
@@ -4,3 +4,16 @@ from rest_framework import generics, permissions
 from .models import Category
 from .permissions import IsAdminUserOrReadOnly
 from .serializers import CategorySerializer
+
+
+# -------------------------
+# User Endpoints
+# -------------------------
+class CategoryListView(generics.ListAPIView):
+    """
+    Returns a list of all active categories.
+    Only active categories are visible to the end user.
+    """
+
+    serializer_class = CategorySerializer
+    queryset = Category.objects.filter(is_active=True)
