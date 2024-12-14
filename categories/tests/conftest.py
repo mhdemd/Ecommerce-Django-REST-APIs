6ad1085debc742bd1196c7c11dd0441756a156eb
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-14 12:38:16 +0330

    create user in conf test

--- categories/tests/conftest.py
+++ categories/tests/conftest.py
@@ -5,3 +5,10 @@ from rest_framework.test import APIClient
 from categories.models import Category
 
 User = get_user_model()
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(
+        email="user@example.com", password="testpassword", is_active=True
+    )
