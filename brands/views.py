ed3e25d685f8a5de27e59f3f27d400c6ee4f1724
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-15 19:02:24 +0330

    add views.py to brand app

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import filters, permissions, viewsets
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from .models import Brand
+from .serializers import BrandSerializer
+
+
+class BrandPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = BrandPagination
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ["name", "created_at"]
+    search_fields = ["name", "description", "slug"]
