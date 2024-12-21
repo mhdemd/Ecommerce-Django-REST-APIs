2c2c577d7b7207a630ec4f3ac8bcfc7a93220f36
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-21 19:25:48 +0330

    add CartSerializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -17,3 +17,22 @@ class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ["id", "product_id", "product_name", "quantity", "product_price"]
+
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+    total_amount = serializers.DecimalField(
+        max_digits=10, decimal_places=2, read_only=True
+    )
+
+    class Meta:
+        model = Cart
+        fields = ["id", "status", "items", "total_amount", "created_at", "updated_at"]
+        read_only_fields = [
+            "id",
+            "status",
+            "items",
+            "total_amount",
+            "created_at",
+            "updated_at",
+        ]
