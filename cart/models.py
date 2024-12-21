e4a0bf5d0c4cbc498d73f1481b270009ac243d86
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-21 19:16:47 +0330

    add CartStatus class

--- cart/models.py
+++ cart/models.py
@@ -3,3 +3,10 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from products.models import Product
+
+
+class CartStatus(models.TextChoices):
+    ACTIVE = "active", _("Active")
+    CHECKOUT = "checkout", _("Checkout")
+    COMPLETED = "completed", _("Completed")
+    CANCELED = "canceled", _("Canceled")
