f8a485d4024b19dcc9be68bb7ecf331ef07087b6
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-21 19:49:48 +0330

    + TestCartViewSet to cart tests

--- cart/tests/test_views.py
+++ cart/tests/test_views.py
@@ -7,3 +7,16 @@ from rest_framework_simplejwt.tokens import AccessToken
 from products.models import Product
 
 User = get_user_model()
+
+
+@pytest.mark.django_db
+class TestCartViewSet:
+    def setup_method(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            email="test@example.com", password="testpass"
+        )
+        access = AccessToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {access}")
+        self.product = Product.objects.create(name="Test Product", price=100)
+        self.product2 = Product.objects.create(name="Another Product", price=50)
