f42d9398b57d9c13bb78babd602c63b0d5887fe3
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-21 19:25:29 +0330

    add CartItemSerializer

--- cart/serializers.py
+++ cart/serializers.py
@@ -3,3 +3,17 @@ from rest_framework import serializers
 from products.models import Product
 
 from .models import Cart, CartItem
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(), source="product", write_only=True
+    )
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    product_price = serializers.DecimalField(
+        source="price", max_digits=10, decimal_places=2, read_only=True
+    )
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product_id", "product_name", "quantity", "product_price"]
