97a9a6705a2b0e6eb57a1afeaa5a39296233f9c9
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-22 21:49:19 +0330

    update urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -1,32 +1,17 @@
 from django.urls import path
 
-from .views import CartViewSet
+from .views import (
+    CartAddItemView,
+    CartCheckoutView,
+    CartListView,
+    CartRemoveItemView,
+    CartUpdateItemView,
+)
 
 urlpatterns = [
-    # GET: List of items in the shopping cart
-    path("", CartViewSet.as_view({"get": "list"}), name="cart-list"),
-    # POST: Add item to cart
-    path(
-        "add-item/",
-        CartViewSet.as_view({"post": "add_item"}),
-        name="cart-add-item",
-    ),
-    # POST: Remove item from cart
-    path(
-        "remove-item/",
-        CartViewSet.as_view({"post": "remove_item"}),
-        name="cart-remove-item",
-    ),
-    # POST: Update the quantity of an item
-    path(
-        "update-item/",
-        CartViewSet.as_view({"post": "update_item"}),
-        name="cart-update-item",
-    ),
-    # POST: Shopping cart checkout
-    path(
-        "checkout/",
-        CartViewSet.as_view({"post": "checkout"}),
-        name="cart-checkout",
-    ),
+    path("", CartListView.as_view(), name="cart-list"),
+    path("add-item/", CartAddItemView.as_view(), name="cart-add-item"),
+    path("remove-item/", CartRemoveItemView.as_view(), name="cart-remove-item"),
+    path("update-item/", CartUpdateItemView.as_view(), name="cart-update-item"),
+    path("checkout/", CartCheckoutView.as_view(), name="cart-checkout"),
 ]
