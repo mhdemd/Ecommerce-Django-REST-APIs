88e86cd07bfb49b1930bdb5747f59cc286dc56a4
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-22 21:40:13 +0330

    Using old routes

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,32 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 
 from .views import CartViewSet
 
-router = DefaultRouter()
-router.register("cart", CartViewSet, basename="cart")
-
-urlpatterns = router.urls
+urlpatterns = [
+    # GET: List of items in the shopping cart
+    path("", CartViewSet.as_view({"get": "list"}), name="cart-list"),
+    # POST: Add item to cart
+    path(
+        "add-item/",
+        CartViewSet.as_view({"post": "add_item"}),
+        name="cart-add-item",
+    ),
+    # POST: Remove item from cart
+    path(
+        "remove-item/",
+        CartViewSet.as_view({"post": "remove_item"}),
+        name="cart-remove-item",
+    ),
+    # POST: Update the quantity of an item
+    path(
+        "update-item/",
+        CartViewSet.as_view({"post": "update_item"}),
+        name="cart-update-item",
+    ),
+    # POST: Shopping cart checkout
+    path(
+        "checkout/",
+        CartViewSet.as_view({"post": "checkout"}),
+        name="cart-checkout",
+    ),
+]
