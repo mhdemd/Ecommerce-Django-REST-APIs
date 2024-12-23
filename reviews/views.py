7296cf3c81b2aeda97bba992ffc5e34e56931418
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-23 12:10:52 +0330

    List all approved reviews for a specific product

--- reviews/views.py
+++ reviews/views.py
@@ -4,3 +4,16 @@ from rest_framework.views import APIView
 
 from .models import Comment, Review, ReviewVote
 from .serializers import CommentSerializer, ReviewSerializer, ReviewVoteSerializer
+
+
+class ReviewListView(generics.ListAPIView):
+    """
+    List all approved reviews for a specific product
+    """
+
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        product_id = self.kwargs.get("product_id")
+        return Review.objects.filter(product_id=product_id, is_approved=True)
