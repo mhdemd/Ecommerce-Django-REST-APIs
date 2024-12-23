c8ec815d86e032a082e1bf085097966bfac89556
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-23 12:10:27 +0330

    add views.py - reviews import

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Comment, Review, ReviewVote
+from .serializers import CommentSerializer, ReviewSerializer, ReviewVoteSerializer
