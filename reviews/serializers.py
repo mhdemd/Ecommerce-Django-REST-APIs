fad136f628488619d557017192dce372fc52a49e
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-23 13:19:52 +0330

    + Serializer for creating, updating, and retrieving comments

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -41,3 +41,30 @@ class ReviewVoteSerializer(serializers.ModelSerializer):
         model = ReviewVote
         fields = ["id", "user", "review", "is_upvote"]
         read_only_fields = ["user"]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for creating, updating, and retrieving comments
+    """
+
+    user_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "user",
+            "user_name",
+            "review",
+            "body",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["user", "created_at", "updated_at"]
+
+    def get_user_name(self, obj):
+        """
+        Return the username of the commenter
+        """
+        return obj.user.username
