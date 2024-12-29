5e6b829973487b199ff465a5d2e09af10a7ef7af
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-29 21:17:38 +0330

    add OrderItem to orders.model

--- orders/models.py
+++ orders/models.py
@@ -26,3 +26,13 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} - {self.user.email} - {self.status}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} x {self.quantity} in Order {self.order.id}"
