2e0a93ef9f9c8814fa321d075025e7d9fbf9086f
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-29 11:57:28 +0330

    add OrderStatus to orders model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+
+
+class OrderStatus(models.TextChoices):
+    PENDING = "pending", "Pending"
+    PAID = "paid", "Paid"
+    SHIPPED = "shipped", "Shipped"
+    COMPLETED = "completed", "Completed"
+    CANCELED = "canceled", "Canceled"
