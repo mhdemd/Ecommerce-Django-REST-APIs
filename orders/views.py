ec7bb7092a5ad44851d428254e6915d9ce1840bf
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-29 21:36:59 +0330

    add views.py to orders App

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Order
+from .serializers import OrderSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        # Return order only for the logged-in user
+        user = self.request.user
+        if user.is_authenticated:
+            return Order.objects.filter(user=user)
+        return Order.objects.none()
