97ad116470af83f9a89a605f4a19a9aab3bf7fa8
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-29 21:29:19 +0330

    + OrderSerializer to orders serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -7,3 +7,19 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ["id", "product", "quantity", "price"]
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    item = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        field = [
+            "id",
+            "user",
+            "status",
+            "total_amount",
+            "craeted_at",
+            "updated_at",
+            "items",
+        ]
