5d8361c2f3b0cb2d2ae4246a89a3f48b2f52526f
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-29 21:57:17 +0330

    add @extend_schema  to orders > views.py

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import get_object_or_404
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import generics
 
 from .models import Order, OrderItem
 from .serializers import OrderItemSerializer, OrderSerializer
 
 
-# View for listing and creating orders
+@extend_schema(
+    summary="List and Create Orders",
+    description="Retrieve a list of orders for the authenticated user or create a new order.",
+    responses={200: OrderSerializer, 201: OrderSerializer},
+)
 class OrderListView(generics.ListCreateAPIView):
     serializer_class = OrderSerializer
 
@@ -18,7 +23,11 @@ class OrderListView(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
-# View for retrieving, updating, and deleting a specific order
+@extend_schema(
+    summary="Retrieve, Update, or Delete an Order",
+    description="Retrieve, update, or delete a specific order for the authenticated user.",
+    responses={200: OrderSerializer},
+)
 class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
 
@@ -27,7 +36,14 @@ class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
         return Order.objects.filter(user=self.request.user)
 
 
-# View for listing and creating order items
+@extend_schema(
+    summary="List and Create Order Items",
+    description="Retrieve a list of items for a specific order or add a new item to the order.",
+    parameters=[
+        OpenApiParameter("order_id", int, description="ID of the order"),
+    ],
+    responses={200: OrderItemSerializer, 201: OrderItemSerializer},
+)
 class OrderItemListView(generics.ListCreateAPIView):
     serializer_class = OrderItemSerializer
 
@@ -44,7 +60,15 @@ class OrderItemListView(generics.ListCreateAPIView):
         serializer.save(order=order)
 
 
-# View for retrieving, updating, and deleting a specific order item
+@extend_schema(
+    summary="Retrieve, Update, or Delete an Order Item",
+    description="Retrieve, update, or delete a specific item within an order.",
+    parameters=[
+        OpenApiParameter("order_id", int, description="ID of the order"),
+        OpenApiParameter("pk", int, description="ID of the order item"),
+    ],
+    responses={200: OrderItemSerializer},
+)
 class OrderItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = OrderItemSerializer
 
