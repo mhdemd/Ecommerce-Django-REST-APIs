c67d10e3d639d1ceeab4a34440a6f3be54345161
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-29 12:07:29 +0330

    add Order model

--- orders/models.py
+++ orders/models.py
@@ -10,3 +10,19 @@ class OrderStatus(models.TextChoices):
     SHIPPED = "shipped", "Shipped"
     COMPLETED = "completed", "Completed"
     CANCELED = "canceled", "Canceled"
+
+
+class Order(models.Model):
+    useer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders"
+    )
+    status = models.CharField(
+        max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING
+    )
+
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user.email} - {self.status}"
