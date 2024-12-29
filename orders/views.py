34e4bfcf6f074c9c48421ae54c5a7784f6c0fb3e
MehdiEmadi <mahdi.emadi@yahoo.com>
2024-12-29 21:44:19 +0330

    + OrderDetailView

--- orders/views.py
+++ orders/views.py
@@ -1,16 +1,18 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
-from .models import Order
-from .serializers import OrderSerializer
+from .models import Order, OrderItem
+from .serializers import OrderItemSerializer, OrderSerializer
 
 
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+# View for listing and creating orders
+class OrderListView(generics.ListCreateAPIView):
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        # Return orders only for the logged-in user
-        user = self.request.user
-        if user.is_authenticated:
-            return Order.objects.filter(user=user)
-        return Order.objects.none()
+        # Only return orders for the authenticated user
+        return Order.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically associate the order with the authenticated user
+        serializer.save(user=self.request.user)
