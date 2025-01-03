33f21882ae95fac65c177a0894f408f9d3cc6e70
MehdiEmadi <mahdi.emadi@yahoo.com>
2025-01-03 22:44:33 +0330

    add WishlistDetailView to wishlist app views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -17,3 +17,15 @@ class WishlistListCreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class WishlistDetailView(generics.RetrieveDestroyAPIView):
+    """
+    Handle retrieving and deleting a single wishlist item for the authenticated user.
+    """
+
+    serializer_class = WishlistSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(user=self.request.user)
