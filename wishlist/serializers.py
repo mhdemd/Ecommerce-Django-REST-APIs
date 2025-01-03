394e5df5efa24a9c62fc810d67a8ac1a89f5f4ba
MehdiEmadi <mahdi.emadi@yahoo.com>
2025-01-03 22:34:10 +0330

    add serializer to wishlist app

---
+++ wishlist/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Wishlist
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = ["id", "user", "product", "added_at"]
+        read_only_fields = ["id", "added_at"]
