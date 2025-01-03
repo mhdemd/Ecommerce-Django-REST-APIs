a33c69026d78e77cebdefe155519ad89d3058fe1
MehdiEmadi <mahdi.emadi@yahoo.com>
2025-01-03 22:44:06 +0330

    add WishlistListCreateView

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Wishlist
+from .serializers import WishlistSerializer
+
+
+class WishlistListCreateView(generics.ListCreateAPIView):
+    """
+    Handles listing and creating wishlist items for the authenticated user.
+    """
+
+    serializer_class = WishlistSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
